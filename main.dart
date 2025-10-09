abd936cf27bbb5dc77dc260863b935b346a900a0
Hafiz Muhammad Hamza Hashmi <hashmihamza894@gmail.com>
2025-10-09 22:54:14 +0500

    Add files via upload
    Lab 06 task

---
+++ main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Container"),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: const Center(
+        child: ContainerWithBoxDecorationWidget(),
+      ),
+    );
+  }
+}
+
+class ContainerWithBoxDecorationWidget extends StatelessWidget {
+  const ContainerWithBoxDecorationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min, // keeps widget compact
+      children: <Widget>[
+        Container(
+          height: 100.0,
+          decoration: BoxDecoration(
+            borderRadius: const BorderRadius.only(
+              bottomLeft: Radius.circular(100.0),
+              bottomRight: Radius.circular(10.0),
+            ),
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.white,
+                Colors.lightGreen.shade500,
+              ],
+            ),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.grey,
+                blurRadius: 10.0,
+                offset: Offset(0.0, 10.0),
+              ),
+            ],
+          ),
+          child: Center(
+            child: RichText(
+              text: TextSpan(
+                text: 'Flutter World',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.deepPurple,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.deepPurpleAccent,
+                  decorationStyle: TextDecorationStyle.dotted,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.normal,
+                ),
+                children: <TextSpan>[
+                  TextSpan(
+                    text: ' for',
+                  ),
+                  TextSpan(
+                    text: ' Mobile',
+                    style: TextStyle(
+                      color: Colors.deepOrange,
+                      fontStyle: FontStyle.normal,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
